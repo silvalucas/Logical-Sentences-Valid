6ef917994e7b84c8fa6692e2449838258df7db58
Lucas Oliveira <l_oliveiras@hotmail.com>
2020-08-27 11:38:29 -0300

    chore(server): Inserts a server to run in heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import Home from './views/home';
+import React from "react";
+import Home from "./views/home";
 
 function App() {
-  return (
-      <Home />
-  );
+  return <Home />;
 }
 
 export default App;
